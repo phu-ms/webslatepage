5aaa1e0f4c8809ca6b1e4b4ecb20dc2097bd15c8
Phu Le <phle@microsoft.com>
2023-11-30 12:31:40 -0800

    update memory leak

--- MemoryLeak/worker.js
+++ MemoryLeak/worker.js
@@ -1,7 +1,8 @@
+let workerData = {};
+
 self.onmessage = function(e) {
     if (e.data.action === 'allocate') {
-        // Allocate memory in the worker
-        let largeData = new Array(e.data.size).join('MemoryLeak ');
-        // You could do something with largeData here
+        // Creating a deeply nested structure in the worker
+        workerData = { data: new Array(e.data.size).join('MemoryLeak '), next: workerData };
     }
 };
